f4e6e0bfbd2897af96be13b0c0ec0b0889f764ff
XiaoPan <2489964425@qq.com>
2021-03-20 21:35:14 +0800

    解决icon应入问题，并封装Icon组件

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,20 @@
+// 引入 path 模块
+const path = require('path')
+
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  chainWebpack: config => {
+    // icon 存在的目录，__dirname表示当前目录,后面就是具体路径
+    const dir = path.resolve(__dirname,'src/assets/icons')
+    // config 就是vue把webpack API封装暴露给我们的一个对象
+    config.module
+      .rule('svg-sprite')
+      .test(/\.svg$/)
+      .include.add(dir).end() // 只包含 icons 目录
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({extract:false}).end()
+    config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'),[{plainSprite: true}])
+    config.module.rule('svg').exclude.add(dir) // 其他 svg loader 排除 icons目录
+  }
 }
