c86fc59a3066af9f08cfbed258d0afdfe2142626
XiaoPan <2489964425@qq.com>
2021-03-21 14:25:49 +0800

    使用淘宝的 viewport

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,14 @@
 <template>
   <nav>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="label"></Icon>
       标签
     </router-link>
-    <router-link to="/money" class="item selected">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"></Icon>
       记账
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"></Icon>
       统计
     </router-link>
@@ -40,7 +40,7 @@ nav {
       height: 28px;
     }
   }
-  > .selected{
+  > .item.selected{
     color: #f60;
   }
 }
