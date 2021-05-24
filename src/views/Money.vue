ddf15f0047d58dc695995a6efc496aefb4df5920
XiaoPan <2489964425@qq.com>
2021-05-25 00:19:33 +0800

    Money.vue html 结构

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,7 +1,29 @@
 <template>
   <div>
     <Layout>
-      <p>Money.vue</p>
+      <div class="tags">
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+      </div>
+      <div>
+        <label>
+          <span class="name">备注</span>
+          <input type="text">
+        </label>
+      </div>
+      <div>
+        <ul class="a">
+
+        </ul>
+      </div>
+      <div>number pad</div>
     </Layout>
   </div>
 </template>
