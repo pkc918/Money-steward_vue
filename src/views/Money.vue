ae11e077da3302d7f4f92eda70c6abd9c5220e02
XiaoPan <2489964425@qq.com>
2021-06-01 09:02:36 +0800

    Money-标签样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -55,5 +55,29 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 
+.tags{
+  font-size: 14px;
+  padding: 16px;
+  > .current{
+    display: flex;
+    >li{
+      background: #d9d9d9;
+      height: 24px;
+      border-radius: (24px/2);
+      padding: 0 16px;
+      margin-right: 12px;
+    }
+  }
+  > .new{
+    padding-top: 16px;
+    button{
+      background: transparent;
+      border: none;
+      color: #999;
+      border-bottom: 1px solid;
+    }
+  }
+}
 </style>
