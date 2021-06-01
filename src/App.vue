ed616ec4e886b43dd3c1447cd97eecdffb5f55ac
XiaoPan <2489964425@qq.com>
2021-06-01 08:47:42 +0800

    添加全局样式喝全局变量

--- src/App.vue
+++ src/App.vue
@@ -6,10 +6,15 @@
 
 <style lang="scss">
 @import "~@/assets/style/reset.scss";
+@import "~@/assets/style/helper.scss";
 
 body{
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #333;
   line-height: 1.5;
   font-size: 16px;
+  font-family: $font-hei;
 }
 
 </style>
--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -23,6 +23,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
 nav {
   display: flex;
   box-shadow: rgb(0 0 0 / 25%) 0 0 3px;
@@ -41,7 +42,7 @@ nav {
     }
   }
   > .selected{
-    color: #f60;
+    color: $color-highlight;
   }
 }
 </style>
