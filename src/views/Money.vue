49f251dc1b744e7bdf5510733da493199476864b
XiaoPan <2489964425@qq.com>
2021-06-02 16:56:04 +0800

    Money-备注样式

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -12,14 +12,14 @@
           <button>新增标签</button>
         </div>
       </div>
-      <div>
+      <div class="notes">
         <label>
           <span class="name">备注</span>
           <input type="text">
         </label>
       </div>
       <div>
-        <ul class="a">
+        <ul class="types">
           <li class="selected">支出</li>
           <li>收入</li>
         </ul>
@@ -77,6 +77,50 @@ export default {
       border: none;
       color: #999;
       border-bottom: 1px solid;
+    }
+  }
+}
+
+.notes{
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  .name{
+    padding-right: 16px;
+  }
+
+  input{
+    height: 64px;
+    flex-grow: 1;
+    background-color: transparent;
+    border: none;
+    padding-right: 16px;
+  }
+}
+
+.types{
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+
+  > li{
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    
+    &.selected::after{
+      content: '';
+      position: absolute;
+      bottom: 0;
+      width: 100%;
+      height: 4px;
+      background-color: #333;
     }
   }
 }
