82dffd7e25cb95c298695c9575ae7e6a204f96a4
XiaoPan <2489964425@qq.com>
2021-09-02 23:58:44 +0800

    目前存在的问题

--- src/main.ts
+++ src/main.ts
@@ -15,6 +15,10 @@ Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
 Vue.component('Icon', Icon);
 
+/*
+* 1 全局变量太多
+* 2 严重依赖 window
+* */
 // record store
 window.recordList = recordListModel.fetch();
 window.createRecord = (record: RecordItem) => {
